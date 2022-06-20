78833185c0a2e8ce9000c14e2ee20696a0815e7d
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-06-20 11:37:35 +0500

    Update task07_test.dart

--- test/task07_test.dart
+++ test/task07_test.dart
@@ -1,8 +1,8 @@
 import 'package:test/test.dart';
-import '../bin/task01.dart';
+import '../bin/task07.dart';
 
 void main() {
-  test('task01', () {
+  test('task07', () {
     expect(func(86), 2);
   });
 }
